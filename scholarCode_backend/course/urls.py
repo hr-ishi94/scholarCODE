2e3c37f342e86a6fc6bac055a2aba5f09b114efc
hprasad <hrishikeshprasadc@gmail.com>
2024-06-09 20:19:33 +0530

    razorpa_url corrected

--- scholarCode_backend/course/urls.py
+++ scholarCode_backend/course/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('mentor/',views.MentorCourseList,name="mentor_courses"),
     path('mentor/delete/<int:pk>/',views.MentorCourseView.as_view(),name="mentor_courses"),
     path('razorpay_order/',razorpay.PaymentView.as_view(),name='payment_view'),
-    path('razorpay_callback/',razorpay.CallbackView.as_view(),name='callback_view')
+    path('razorpay_callback/',razorpay.CallbackView.as_view(),name='callback_view'),
     
    ]
 
