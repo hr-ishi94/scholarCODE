aee1eef746c4a9288446ed0175810aff48f84f9e
hprasad <hrishikeshprasadc@gmail.com>
2024-06-13 22:06:33 +0530

    channel asgi set up

--- scholarCode_backend/scholarCode_backend/urls.py
+++ scholarCode_backend/scholarCode_backend/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('main/', include('main.urls')),
     path('course/', include('course.urls')),
+    path('chat/', include('chat.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
