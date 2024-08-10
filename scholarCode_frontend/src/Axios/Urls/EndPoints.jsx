349ad926f18a650ed83576d164d0e2632d0693af
hprasad <hrishikeshprasadc@gmail.com>
2024-08-10 06:49:00 +0530

    end points updated

--- scholarCode_frontend/src/Axios/Urls/EndPoints.jsx
+++ scholarCode_frontend/src/Axios/Urls/EndPoints.jsx
@@ -1,16 +1,16 @@
-export const main = 'http://localhost:8000/main/'
+export const main = 'http://13.53.80.235//main/'
 
-export const course = 'http://localhost:8000/course/'
+export const course = ' http://13.53.80.235//course/'
 
-export const course_url = 'http://localhost:8000/course/'
+export const course_url = ' http://13.53.80.235//course/'
 
-export const base_url = 'http://localhost:8000'
+export const base_url = ' http://13.53.80.235/'
 
-export const SOCKET = 'ws://localhost:8000/ws'
+export const SOCKET = 'http://13.53.80.235//ws'
 
-export const CHAT = 'http://localhost:8000/chat/'
+export const CHAT = ' http://13.53.80.235//chat/'
 
-export const Vurl = 'http://localhost:5173/'
+export const Vurl = 'https://scholarcode.vercel.app/'
 
-export const Notification_url = 'http://localhost:8000/notifications/noti/'
+export const Notification_url = ' http://13.53.80.235//notifications/noti/'
 
